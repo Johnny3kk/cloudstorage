f0cae5715114e243e4c85f761a94630e5d5bead6
Johnny3kk <some@mail.ru>
2020-06-18 18:53:30 +0300

    HW1

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,14 +1,26 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    final private int WIDTH = 1000;
+    final private int HEIGHT = 600;
+
+
     public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("view1.fxml"));
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
 
-
+        primaryStage.setTitle("GB-Cloud-Storage");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
